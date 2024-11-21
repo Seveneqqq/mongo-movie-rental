b8d7d645e7a8e6928e5ace487d85195a24e1589b
Paweł Boroń <pawcio2001.boron@interia.pl>
2024-11-21 13:30:59 +0100

    rezygnacja z vercela

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Book = require('./schema/books');
-const User = require('./schema/users');
-
+require('dotenv').config();
 
 const app = express();
-
 app.use(express.json());
 
-app.get('/api', (req, res) => {   // usunięte '/api'
-  res.send('<h1>hello from api</h1>');
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        console.log(`MongoDB Connected`);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
+
+connectDB();
+
+app.get('/api', (req, res) => {   
+    res.send('<h1>hello from api</h1>');
 });
 
 const port = process.env.PORT || 5000;
-app.listen(port);
-
-console.log('hello from backend');
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
